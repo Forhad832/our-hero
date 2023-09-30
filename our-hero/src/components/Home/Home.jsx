e3f0fe1be8b7fd9fbd8fa12ad4a007b205daed48
forhad <forhadhossain0619@gmail.com>
2023-09-30 23:08:39 +0600

    add text content

--- our-hero/src/components/Home/Home.jsx
+++ our-hero/src/components/Home/Home.jsx
@@ -9,8 +9,8 @@ const Home = () => {
             <div className="hero-overlay bg-opacity-60"></div>
             <div className="hero-content text-center text-neutral-content">
                 <div className="max-w-md">
-                    <h1 className="mb-5 text-5xl font-serif font-bold">Our Real Heros</h1>
-                    <p className="mb-5">Our real heros,Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                    <h1 className="mb-5 text-5xl font-serif font-bold">Our Real Heroes</h1>
+                    <p className="mb-5">Our real heroes,Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
 
                 </div>
             </div>
